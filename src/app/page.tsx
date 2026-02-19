caf320ad922a4bf5ffed2da7002ec024ff14b263
dueon <hdejh79@gmail.com>
2026-02-19 01:31:11 -0800

    build test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,41 @@
-import { BrowserRouter } from "react-router-dom";
-import About from "../app/components/About";
-import Tech from "../app/components/Tech";
-import Contact from "../app/components/Contact";
-import Experience from "../app/components/Experience";
-import Feedback from "../app/components/Feedbacks";
-import Hero from "../app/components/Hero";
-import Navbar from "../app/components/Navbar";
-import Works from "../app/components/Works";
-import StartsCanvas from "./components/StartsCanvas";
+"use client";
 
+import dynamic from "next/dynamic";
 import "@/app/globals.css";
+
+const Hero = dynamic(() => import("../app/components/Hero"), { ssr: false });
+const Experience = dynamic(() => import("../app/components/Experience"), {
+  ssr: false,
+});
+const Tech = dynamic(() => import("../app/components/Tech"), { ssr: false });
+const Contact = dynamic(() => import("../app/components/Contact"), {
+  ssr: false,
+});
+const Works = dynamic(() => import("../app/components/Works"), { ssr: false });
+const About = dynamic(() => import("../app/components/About"), { ssr: false });
+const Navbar = dynamic(() => import("../app/components/Navbar"), {
+  ssr: false,
+});
+const StartsCanvas = dynamic(() => import("./components/StartsCanvas"), {
+  ssr: false,
+});
 
 const Page = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover-bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works /> {/*modify*/}
-        {/*<Feedback />*/} {/*modify*/}
-        <div className="relative z-0">
-          <Contact />
-          <StartsCanvas />
-        </div>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
       </div>
-    </BrowserRouter>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <div className="relative z-0">
+        <Contact />
+        <StartsCanvas />
+      </div>
+    </div>
   );
 };
 
